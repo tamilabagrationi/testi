f568c7d4538131f96e57656c005dffe1f4d63a7c
tamilabagrationi <116660917+tamilabagrationi@users.noreply.github.com>
2025-01-29 17:08:08 +0400

    welcom

---
+++ script.js
@@ -0,0 +1,58 @@
+//  var a = 4;
+// var b = 3;
+// var c= a + b;
+// console.log(c);
+
+// var userName ='Nino'
+// console.log(userName)
+// var age =14
+// console.log(age);
+// var is_boy = false;
+// console.log(is_boy)
+
+// let userName1 = 'Ana'
+// userName1 = 'Nata'
+// console.log(typeof age)
+
+// lecrure 2
+
+// var number1 = 22;
+// var number2 = 4;
+// var sum = number1 + number2;
+// console.log(sum);
+
+// let conts
+// Array  მასივი
+
+// let fruits = ["apple", "banana", "cherry"];
+// fruits.push("orange");
+// console.log(fruits);
+
+// let numbers = [10, 20, 30, 40];
+// numbers.shift()
+// console.log(numbers)
+
+// let colors = ["red", "blue", "green"];
+// colors.splice(1, 0, "yellow");
+// console.log(colors);
+
+// let animals1 = ["cat", "dog"];
+// let animals2 = ["lion", "tiger"];
+// let animals=animals1.concat(animals2);
+// console.log(animals)
+
+// let scores = [50, 30, 20, 10, 40];
+// scores.sort()
+// console.log(scores)
+
+// let cities = ["Tbilisi", "Batumi", "Kutaisi"];
+// let result = cities.join(" - ");
+// console.log(result);
+
+// let letters = ["a", "b", "c", "d"];
+// letters.reverse();
+// console.log(letters);
+
+let books = ["Math", "Science", "History"];
+let length = books.length;
+console.log(length);
